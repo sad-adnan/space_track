4af4bc53aa0947e7df194dfd7a13eed1243258c8
MD. Sad Adnan <mail2sad.bd@gmail.com>
2025-03-24 20:21:51 +0600

    TASK-3: Created the AppConfig class for managing environments and added env files path in pubspec.yaml.

---
+++ lib/core/app_config.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+enum AppFlavor { prod ,staging ,dev}
+
+class AppConfig {
+
+  static const AppFlavor dev = AppFlavor.dev;
+  static const AppFlavor staging = AppFlavor.staging;
+  static const AppFlavor prod = AppFlavor.prod;
+
+  static AppFlavor _environment = dev;
+  static AppFlavor get environment => _environment;
+
+  static Future<void> initializeEnvironment(AppFlavor env) async {
+    _environment = env;
+
+    String fileName;
+    switch (env) {
+      case dev:
+        fileName = '.env.dev';
+        break;
+      case staging:
+        fileName = '.env.staging';
+        break;
+      case prod:
+        fileName = '.env';
+        break;
+      default:
+        fileName = '.env.dev';
+    }
+
+    await dotenv.load(fileName: fileName);
+  }
+
+
+  static String? getEnvironmentValue(String key) {
+    return dotenv.env[key];
+  }
+
+  static bool get isDev => _environment == dev;
+
+  static bool get isStaging => _environment == staging;
+
+  static bool get isProd => _environment == prod;
+}

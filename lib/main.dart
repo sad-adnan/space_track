6cba14b6fa3aeecfafc2b7db4f26fb5414addb62
MD. Sad Adnan <mail2sad.bd@gmail.com>
2025-03-24 20:33:38 +0600

    TASK-3: Set up multiple environments with main_dev, main_staging, and main_prod, and initialized environment and added environment name to the app title.

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:scandit_flutter_datacapture_barcode/scandit_flutter_datacapture_barcode.dart';
+import 'package:space_track/core/app_config.dart';
 import 'package:space_track/core/pallets.dart';
 
 import 'core/app_pages.dart';
 import 'core/app_route_constants.dart';
 import 'data/models/packing_info_model.dart';
-import 'generated/assets.dart';
 
 void main() async {
+  //if no flavor is passed we will use the dev flavor
+  await mainInitializations(AppFlavor.dev);
+}
+
+Future<void> mainInitializations(AppFlavor environment) async {
   // Ensure Flutter is initialized
   WidgetsFlutterBinding.ensureInitialized();
 
-  //Initialize the Scandit Library
-  await ScanditFlutterDataCaptureBarcode.initialize();
+  // Initialize the environment
+  await AppConfig.initializeEnvironment(environment);
 
-  // Initialize environment variables
-  await dotenv.load(fileName: Assets.env);
+  // Initialize the Scandit Library
+  await ScanditFlutterDataCaptureBarcode.initialize();
 
   // Initialize Hive
   await Hive.initFlutter();
@@ -42,7 +46,7 @@ class SpaceTrackApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'SpaceTrack',
+      title: 'SpaceTrack${!AppConfig.isProd ? " - ${AppConfig.environment.name.toUpperCase()}" : ""}',
       theme: ThemeData(
         primaryColor: Pallets.primaryColor,
         appBarTheme: const AppBarTheme(
@@ -54,5 +58,4 @@ class SpaceTrackApp extends StatelessWidget {
       getPages: AppPages.routes,
     );
   }
-
 }
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:space_track/core/app_config.dart';
+import 'main.dart' as main_common;
+
+void main() async {
+  await main_common.mainInitializations(AppFlavor.staging);
+}
